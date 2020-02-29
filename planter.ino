799ddec183fc5480b38a0496bf98893159ef3f15
darkowl91 <Owl5832558>
2020-02-29 19:34:55 +0300

    button press event

--- planter.ino
+++ planter.ino
@@ -2,7 +2,6 @@
  Smart Flower Controller based on LoLin new NodeMcu v3(ESP-12E) board
  Board Manager: http://arduino.esp8266.com/stable/package_esp8266com_index.json
  Upload speed: 115200
- CPU frequency: 80MHz
 
  PINS:
  * A0 - Soil moisture sensor
@@ -16,7 +15,8 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 #include <SimpleDHT.h>
-#include "SSD1306.h"
+#include <SSD1306.h>
+#include <timer.h>
 #include "Secrets.h"
 #include "Images.h"
 
@@ -25,7 +25,7 @@ const int moiPin = 0;  // A0
 const int sdaPin = 5;  // D1
 const int sclPin = 4;  // D2
 const int ledPin = 14; // D5
-const int bntPin = 12; // D6
+const int btnPin = 12; // D6
 const int dhtPin = 13; // D7
 
 // WIFI
@@ -42,9 +42,34 @@ int t; // temperature
 int h; // humidity
 int m; // soil moisture
 
-// sensors intervals
-long lastTimeSensorUpdate = 0;
-int updateInterval = 3600000; // 1h
+// sensors update timer
+auto timer = timer_create_default();
+
+// button state
+int btnState = LOW;
+int btnPressTime = -1;
+
+// display frames
+typedef void (*Frame)(void);
+
+void drawTemperature()
+{
+  display.drawString(0, 0, "Temperature");
+}
+
+void drawHumidity()
+{
+  display.drawString(0, 0, "Humidity");
+}
+
+void drawSoilMoisture()
+{
+  display.drawString(0, 0, "Soil moisture");
+}
+
+Frame frames[] = {drawTemperature, drawHumidity, drawSoilMoisture};
+int frameSize = (sizeof(frames) / sizeof(Frame));
+int frameNum = 0;
 
 void setup()
 {
@@ -54,46 +79,53 @@ void setup()
   // set ledPin mode
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, HIGH);
+  pinMode(btnPin, INPUT);
   // set display
   display.init();
   display.setFont(ArialMT_Plain_10);
   display.flipScreenVertically();
+
+  // setup timer
+  timer.every(3600, updateSensors);
 }
 
 void loop()
 {
-  display.clear();
+  timer.tick();
 
-  updateSensors();
-
-  drawValues();
-  display.display();
-
-  delay(10);
 }
 
-void drawValues()
+bool isBtnPressed()
 {
-  display.drawString(0, 15, "Temperature: " + String(t));
-  display.drawString(0, 25, "Humidity: " + String(h));
-  display.drawString(0, 35, "Moisture: " + String(m));
-}
+  int debounce = 20;
+  bool isPressed = false;
+  int value = digitalRead(btnPin);
 
-void updateSensors()
-{
-  if ((millis() > lastTimeSensorUpdate + updateInterval) || lastTimeSensorUpdate == 0)
+  if (value == HIGH && btnState == LOW && (millis() - btnPressTime > debounce))
   {
-    byte temperature = 0;
-    byte humidity = 0;
-    int err = SimpleDHTErrSuccess;
-    if ((err = dht11.read(&temperature, &humidity, NULL)) == SimpleDHTErrSuccess)
-    {
-      t = (int)temperature;
-      h = (int)humidity;
-    }
-
-    m = analogRead(moiPin);
-
-    lastTimeSensorUpdate = millis();
+    isPressed = true;
+    Serial.println("Button pressed");
+    btnPressTime = millis();
   }
+
+  btnState = value;
+
+  return isPressed;
+}
+
+bool updateSensors(void *)
+{
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) == SimpleDHTErrSuccess)
+  {
+    t = (int)temperature;
+    h = (int)humidity;
+  }
+
+  m = analogRead(moiPin);
+
+  Serial.println("Update sensors -> t: " + String(t) + " h: " + String(h) + " m: " + String(m));
+  return true;
 }
